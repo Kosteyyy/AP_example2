91bf4edc7c39521f5ca6724c80e8bd4e5f7ce21e
Kosteyyy <7456052@gmail.com>
2025-04-13 12:13:40 +0300

    ReactiveForm начало

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -4,7 +4,7 @@ import { MODES, SharedState, StateUpdate } from "./sharedState.service";
 import { MessageService } from "../messages/message.service";
 import { Model } from "../model/repository.model";
 import { Message } from "../messages/message.model";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, NgForm, Validators } from "@angular/forms";
 
 @Component({
     selector: "paForm",
@@ -15,6 +15,22 @@ export class FormComponent {
     product: Product = new Product();
     editing: boolean = false;
 
+    nameField: FormControl = new FormControl("", {
+        validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern("^[A-Za-z ]+$"),
+        ],
+        updateOn: "change",
+    });
+
+    categoryField: FormControl = new FormControl();
+
+    productForm: FormGroup = new FormGroup({
+        name: this.nameField,
+        category: this.categoryField,
+    });
+
     constructor(
         private model: Model,
         private state: SharedState,
@@ -22,6 +38,41 @@ export class FormComponent {
     ) {
         this.state.changes.subscribe((upd) => this.handleStateChange(upd));
         this.messageService.reportMessage(new Message("Creating New Product"));
+    }
+
+    ngOnInit() {
+        this.productForm.statusChanges.subscribe((newStatus) => {
+            if (newStatus == "INVALID") {
+                let invalidControls: string[] = [];
+                for (let controlName in this.productForm.controls) {
+                    if (this.productForm.controls[controlName].invalid) {
+                        invalidControls.push(controlName);
+                    }
+                }
+                this.messageService.reportMessage(
+                    new Message(`INVALID: ${invalidControls.join(", ")}`)
+                );
+            } else {
+                this.messageService.reportMessage(new Message(newStatus));
+            }
+        });
+
+        // this.nameField.statusChanges.subscribe((newStatus) => {
+        //     if (newStatus == "INVALID") {
+        //         this.categoryField.disable();
+        //     } else {
+        //         this.categoryField.enable();
+        //     }
+        // });
+
+        // this.nameField.valueChanges.subscribe((newValue) => {
+        //     this.messageService.reportMessage(
+        //         new Message(newValue || "(Empty)")
+        //     );
+        //     if (typeof newValue == "string" && newValue.length % 2 == 0) {
+        //         this.nameField.markAsPristine();
+        //     }
+        // });
     }
 
     handleStateChange(newState: StateUpdate) {
@@ -34,19 +85,24 @@ export class FormComponent {
             this.messageService.reportMessage(
                 new Message(`Editing ${this.product.name}`)
             );
+            // this.nameField.setValue(this.product.name);
+            // this.categoryField.setValue(this.product.category);
         } else {
             this.product = new Product();
             this.messageService.reportMessage(
                 new Message("Creating New Product")
             );
+            // this.nameField.setValue("");
+            // this.categoryField.setValue("");
         }
+        this.productForm.reset(this.product);
     }
 
-    submitForm(form: NgForm) {
-        if (form.valid) {
-            this.model.saveProduct(this.product);
-            this.product = new Product();
-            form.resetForm();
-        }
-    }
+    // submitForm(form: NgForm) {
+    //     if (form.valid) {
+    //         this.model.saveProduct(this.product);
+    //         this.product = new Product();
+    //         form.resetForm();
+    //     }
+    // }
 }
---
+++ src/app/core/validation_helpers.ts
@@ -0,0 +1,34 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+@Pipe({
+    name: "validationFormat",
+})
+export class ValidationHelper implements PipeTransform {
+    transform(source: any, name: any): any {
+        if (source instanceof FormControl) {
+            return this.formatMessages((source as FormControl).errors, name);
+        }
+        return this.formatMessages(source as ValidationErrors, name);
+    }
+
+    formatMessages(errors: ValidationErrors | null, name: string): string[] {
+        let messages: string[] = [];
+        for (let errorName in errors) {
+            switch (errorName) {
+                case "required":
+                    messages.push(`Вы должны ввести ${name}`);
+                    break;
+                case "minlength":
+                    messages.push(
+                        `${name} должно содержать как минимум ${errors["minlength"].requiredLength} символов`
+                    );
+                    break;
+                case "pattern":
+                    messages.push(`${name} содержит недопустимые символы`);
+                    break;
+            }
+        }
+        return messages;
+    }
+}
