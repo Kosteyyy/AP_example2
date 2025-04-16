385f004056f0df2a1bfc75956751f23f1e1d8978
Kosteyyy <averkiev@yandex.ru>
2025-04-16 11:29:29 +0300

    Rest datasource  и генерация ошибок через подмену сервиса ошибок

--- src/app/messages/message.modlule.ts
+++ src/app/messages/message.modlule.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
+import { ErrorHandler, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { MessageComponent } from "./message.component";
 import { MessageService } from "./message.service";
+import { MessageErrorHandler } from "./errorHandler";
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [MessageComponent],
-    providers: [MessageService],
+    providers: [MessageService, { provide: ErrorHandler, useClass: MessageErrorHandler }],
     exports: [MessageComponent]
 })
 export class MessageModule {}
