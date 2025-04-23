3521f28608d7e8e252fa3fc49f0ad8210c8c9de1
Kosteyyy <averkiev@yandex.ru>
2025-04-23 18:48:12 +0300

    Несколько router-outlet с именами

--- src/app/ondemand/ondemand.module.ts
+++ src/app/ondemand/ondemand.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from "@angular/core";
 import { OndemandComponent } from "./ondemand.component";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { FirstComponent } from "./first.component";
+import { SecondComponent } from "./second.component";
 
 let routing = RouterModule.forChild([
-    { path: "", component: OndemandComponent },
+    {
+        path: "",
+        component: OndemandComponent,
+        children: [
+            { outlet: "primary", path: "", component: FirstComponent },
+            { outlet: "left", path: "", component: SecondComponent },
+            { outlet: "right", path: "", component: SecondComponent },
+        ],
+    },
 ]);
 
 @NgModule({
     imports: [CommonModule, routing],
     exports: [OndemandComponent],
-    declarations: [OndemandComponent],
+    declarations: [OndemandComponent, FirstComponent, SecondComponent],
     providers: [],
 })
 export class OndemandModule {}
